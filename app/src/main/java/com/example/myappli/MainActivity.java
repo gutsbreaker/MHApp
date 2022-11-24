af9e5c4dda85729dcdfa38ddc9106a966b79b934
gutsbreaker <abelidoguts@gmail.com>
2022-11-24 21:01:33 +0800

    HI Clien

--- app/src/main/java/com/example/myappli/MainActivity.java
+++ app/src/main/java/com/example/myappli/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myappli;
 
 import android.os.Bundle;
 
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +18,9 @@ import com.example.myappli.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +31,31 @@ public class MainActivity extends AppCompatActivity {
 
 
         setContentView(R.layout.activity_main);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        //admin and admin
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+
+                    //correct
+
+                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFULL", Toast.LENGTH_SHORT).show();
+
+                }else
+                //incorrect
+                    Toast.makeText(MainActivity.this,"LOGIN FAILED !!!", Toast.LENGTH_SHORT).show();
+            }
+
+
+
+        });
 
 
     }
